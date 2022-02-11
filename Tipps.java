27c191b2ce0560abdaf1abf6e943750f2ff2d158
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2022-02-11 23:31:59 +0100

    Hier für Tipps schauen
    - Changed Formatting

--- Tipps.java
+++ Tipps.java
@@ -2,6 +2,9 @@
 /**
 * @description Eine kleine Hilfestellung zum Anfangen mit der aktuellen Aufgabe zu zufällig generierten Arrays
 * @author JustOneDeveloper
+* 
+* Note: Die Dinger rechts sind für das Auskommentieren meiner kurzen Texte. Wenn ihr die Wegmacht, achtet darauf, dass
+* 	diese Texte auch gone sind.
 */
 
 
@@ -11,7 +14,7 @@
 //Global definiert, also außerhalb von Funktionen und Methoden:
 
 public int[] zahlen = new int[50];		//Neuen Ganzzahlen-Array der Größe 50 machen,
-										//denn wir wollen 50 zufällige Zahlen
+						//denn wir wollen 50 zufällige Zahlen
 
 
 	
@@ -27,23 +30,24 @@ public int maximalerWert = 100;
 //Als nächstes brauchen wir eine Methode, um diese Zahlen zu setzen:
 
 public void zahlenSetzen() {
-	
-	//Als erstes eine for-schleife, die auch 50 mal durchläuft
-	//Dazu also prüfen, ob i kleiner als 50 ist, damit die Schleife 0-49 durchläuft
+																		/*
+	Als erstes eine for-schleife, die auch 50 mal durchläuft
+	Dazu also prüfen, ob i kleiner als 50 ist, damit die Schleife 0-49 durchläuft								*/
 	
 	for(int i = 0; i < 50; i += 1) {
-		
-		//Dann immer aus dem zahlen-array die "i-te" Stelle nehmen
-		//(Also beim ersten Durchlauf 0, dann 1, dann 2 usw. bis 49)
-		//Jetzt kommt die Methode für einen
+																		/*
+		Dann immer aus dem zahlen-array die "i-te" Stelle nehmen
+		(Also beim ersten Durchlauf 0, dann 1, dann 2 usw. bis 49)
+		Jetzt kommt die Methode für einen												*/
 		
 		zahlen[i] = (int) (Math.random() * maximalerWert) + 1;		//Die im Unterricht kennengelernte Random-Formel
 		
-		
-//		Hier müsst ihr anschließend noch einbauen, dass keine Zahlen doppelt vorkommen.
-//		Das macht ihr, indem ihr alle Zahlen, die bisher schon generiert wurden (lässt sich an unserem i erkennen)
-//		überprüft. Dann schaut ihr, ob es die gleiche Zahl ist wie die, die jetzt generiert wurde (zahlen[i]).
-//		Falls ja, müsst ihr den Schleifendurchlauf nochmal machen.
+																		/*
+		Hier müsst ihr anschließend noch einbauen, dass keine Zahlen doppelt vorkommen.
+		Das macht ihr, indem ihr alle Zahlen, die bisher schon generiert wurden (lässt sich an unserem i erkennen)
+		überprüft. Dann schaut ihr, ob es die gleiche Zahl ist wie die, die jetzt generiert wurde (zahlen[i]).
+		Falls ja, müsst ihr den Schleifendurchlauf nochmal machen.									*/
+
 
 //		Bei irgendwelchen Fragen kontaktiert mich einfach.
 		
